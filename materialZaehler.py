bfbd8c243e63146214ec45178deaacb08e88fe68
Marvin Niehüser <marvinns105@gmail.com>
2026-02-18 11:51:45 +0100

    Start des Projekt

---
+++ materialZaehler.py
@@ -0,0 +1,9 @@
+"""
+material eintragen mit menge in listen und menue aufrzfbar : eintragen, einsehen, entfernen, beenden
+"""
+listevonMaterial = {}
+
+materialname = input("welches materieal möchtest du eintragen: ")
+materialmenge = input(f"wie viel {materialname} möchtest du eintragen: ")
+
+listevonMaterial["Name"] = materialname 
