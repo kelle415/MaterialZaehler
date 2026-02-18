94464c455bc9b57d1ca7f000b556bfc310b3a3f7
Marvin Niehüser <marvinns105@gmail.com>
2026-02-18 16:13:06 +0100

    Erweitert um Struktur undJson

--- materialZaehler.py
+++ materialZaehler.py
@@ -1,9 +1,42 @@
 """
 material eintragen mit menge in listen und menue aufrzfbar : eintragen, einsehen, entfernen, beenden
 """
-listevonMaterial = {}
 
-materialname = input("welches materieal möchtest du eintragen: ")
-materialmenge = input(f"wie viel {materialname} möchtest du eintragen: ")
+import json
 
-listevonMaterial["Name"] = materialname 
+def baustellenListedatenLaden():
+    with open("baustellenListe.json","r", encoding="utf-8") as f:
+        return json.load(f)
+
+def baustellendatenspeichern(baustellenListe):
+    with open("baustellenListe.json", "w",encoding="utf-8") as f:
+        json.dump(baustellenListe,f,indent=4, ensure_ascii=False)
+
+def baustelleAbfragen():
+    baustellenInput = input("Auf welcher baustelle bist du im moment: ")
+    return baustellenInput
+
+def materialUndMengeAbfrage():
+    materialname = input("welches material möchtest du eintragen: ")
+    materialmenge = input(f"wie viel {materialname} möchtest du eintragen: ")
+    return materialname,materialmenge
+
+def baustellendatenzusammenstellen(baustellenInput,materialname,materialmenge):
+    baustellenListe[baustellenInput]["Material"].setdefault(materialname, {"Menge":materialmenge})
+    baustellendatenspeichern(baustellenListe)
+
+def ablaufplan():
+    baustellenInput = baustelleAbfragen()
+    materialname, materialmenge = materialUndMengeAbfrage()
+    baustellendatenzusammenstellen(
+        baustellenInput,
+        materialname,
+        materialmenge
+    )
+    print(baustellenListe)
+
+baustellenListe = baustellenListedatenLaden()
+
+ablaufplan()
+
+
