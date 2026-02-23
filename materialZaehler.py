2579fba1073953bd9e8f3de469124e5af0f45e54
Marvin Niehüser <marvinns105@gmail.com>
2026-02-23 20:56:43 +0100

    Material Bearbeitungsmenü Hinzugefügt

--- materialZaehler.py
+++ materialZaehler.py
@@ -1,7 +1,6 @@
 """
-material eintragen mit menge in listen und menue aufrzfbar : eintragen, einsehen, entfernen, beenden
+material eintragen mit menge in json und menue aufrufbar: eintragen, einsehen, entfernen, beenden
 
-menue aufbau verbessern, 
 """
 
 import json
@@ -51,19 +50,19 @@ def baustellendatenzusammenstellen(baustellenInput,materialname,materialmenge,ma
     baustellendatenspeichern(baustellenListe)
 
 def menueverweis():
+    print("\nWas möchtest du heute machen? \n 1. Material eintragen\n 2. Material Liste anzeigen\n 3. Material ändern\n 4. Beenden")
+    menue = str(input("\nAntwort: "))
     baustellenInput= baustelleAbfragen()
-    print("was möchtest du heute machen? \n 1. Material eintragen\n 2. Material Liste anzeigen\n 3. Material ändern\n 4. Beenden")
-    menue = str(input("Antwort: "))
     if menue in ("eintragen","Material eintragen","1"):
-        materialEintragen()
+        materialEintragen(baustellenInput)
         zurueck()
     elif menue in ("Liste anzeigen","Material anzeigen","2"):
         materialAnzeigen(baustellenListe,baustellenInput)
         zurueck()
     elif menue in ("aus liste entfernen","Material ändern","3"):
-        materialAendernabfragen(baustellenInput)
+        allgemeinAendernabfragen(baustellenListe,baustellenInput)
         zurueck()
-    elif menue in ("Beenden","4"):
+    if menue in ("Beenden","4"):
         beenden()
         
 def materialEintragen(baustellenInput):
@@ -73,7 +72,6 @@ def materialEintragen(baustellenInput):
 def materialAnzeigen(baustellenListe,baustellenInput):
     print("\n","-" * 25)
     print(f" Baustelle: {baustellenInput}")
-
     material = baustellenListe[baustellenInput].get("Material", {})
     if not material:
         print("Kein Material vorhanden")
@@ -84,43 +82,106 @@ def materialAnzeigen(baustellenListe,baustellenInput):
         einheit = info["Einheit"]
         print(f"- {name}: {menge} {einheit}")
     print("-" * 25,"\n")
+def bennung():
+    "hier soll dann mitgegebne werden ob tembaustellen oder tempmaterial oder tempmenge angezeigt wird"
 
-def materialAendernabfragen(baustellenInput):
+def allgemeinAendernabfragen(baustellenListe,baustellenInput):
     temporaereListeMaterial = temporaereListeMaterialerstellen(baustellenListe,baustellenInput)
     temporaereListeBaustelle = temporaereListeBaustelleerstellen(baustellenListe)
     temporaereListeEinheit,temporaereListeMenge = temporaereListeMenge_temporaereListeEinheit_erstellen(baustellenListe,baustellenInput)
     materialAnzeigen(baustellenListe,baustellenInput)
     print("was möchtest du ändern ?")
     print(f"\n 1. Baustellennamen{temporaereListeBaustelle},\n 2. Material{temporaereListeMaterial},\n 3. Menge{temporaereListeMenge},\n 4. Einheiten,\n 5. Nichts(Beenden)")
-    abfrage = input("\n")
+    abfrage = input("\nAntwort: ")
     if abfrage in ("1","Baustellennamen","baustellen"):
-        baustelleAendernabfragen(abfrage)
-    elif abfrage in temporaereListeMaterial:
-        pass
-    elif abfrage in temporaereListeMenge:
-        pass
+        baustelleAendernabfragen(baustellenListe,baustellenInput,abfrage, temporaereListeBaustelle)
+    elif abfrage in ("2","Material"):
+        materialAendernabfragen(baustellenListe,baustellenInput,abfrage, temporaereListeMaterial)
+    elif abfrage in ("3","Menge","menge"):
+        mengeAendernabfragen(baustellenListe,baustellenInput,abfrage,temporaereListeMaterial,temporaereListeMenge)
     elif abfrage in temporaereListeEinheit:
         pass
     else: 
         print("unverwertbare eingabe")
 
-def baustelleAendernabfragen(abfrage):
-    print(f"Möchtest du hierraus {abfrage} etwas ändern? (J/N)")
-    aenderninput= input("\n")
+def baustelleAendernabfragen(baustellenListe,baustellenInput,abfrage,temporaereListeBaustelle):
+    print(f"\nMöchtest du hierraus {abfrage}: {temporaereListeBaustelle} etwas ändern? (J/N)")
+    aenderninput= input("Antwort: ")
     if aenderninput == "J":
-        aendern()
+        modus = modus1(False)
+        zuaendern,geandert,sicherheitsfrage,materialname = aendernabfragen(modus)
+        baustellennamenaendern(baustellenListe,baustellenInput,zuaendern,geandert,sicherheitsfrage,materialname)
+    elif aenderninput == "N":
+        zurueck()
 
-def aendern():
-    geaendert = input("Bitte gib den geänderten namen ein: ")
-    sicherheitsfrage = input(f"ist {geaendert} richtig geschrieben? (J/N)")
+def materialAendernabfragen(baustellenListe,baustellenInput,abfrage,temporaereListeMaterial):
+    print(f"\nMöchtest du hierraus {abfrage}: {temporaereListeMaterial} etwas ändern? (J/N)")
+    aenderninput= input("Antwort: ")
+    if aenderninput == "J":
+        modus = modus1(False)
+        zuaendern,geandert,sicherheitsfrage,materialname = aendernabfragen(modus)
+        materialnamenaendern(baustellenListe,baustellenInput,zuaendern,geandert,sicherheitsfrage,materialname)
+    elif aenderninput == "N":
+        zurueck()
+
+def mengeAendernabfragen(baustellenListe,baustellenInput,abfrage,temporaereListeMaterial,temporaereListeMenge):
+    print(f"\nMöchtest du hierraus {abfrage}: {temporaereListeMaterial}:{temporaereListeMenge} etwas ändern? (J/N)")
+    aenderninput = input("Antwort: ")
+    if aenderninput =="J":
+        modus= modus1(True)
+        zuaendern,geandert,sicherheitsfrage,materialname = aendernabfragen(modus)
+        mengenaendern(baustellenListe,baustellenInput,zuaendern,geandert,sicherheitsfrage,materialname)
+    elif aenderninput == "N":
+        zurueck()
+
+def modus1(daten):
+    if daten:
+        return "menge"
+    else:
+        None
+
+def aendernabfragen(modus):
+    materialname = None
+    if modus == "menge":
+        print("wie ist der zugehörige material name? ")
+        materialname = input("Antwort: ")
+
+    zuaendern = input("Bitte gib den zuändernden namen ein: ")
+    print("\nWie soll der neue name heißen ? ")
+    geandert = input("Antwort: ")
+    print(f"\nBist du sicher das {zuaendern} zu {geandert} geändert werden soll? (J/N)")
+    sicherheitsfrage = input("Antwort: ")
+    return zuaendern,geandert,sicherheitsfrage,materialname
+
+def baustellennamenaendern(baustellenListe,baustellenInput,zuaendern,geandert,sicherheitsfrage,materialname):
     if sicherheitsfrage == "J":
-        wert = ""
-        baustellenListe[geaendert]
+        if zuaendern in baustellenListe: 
+            baustellenListe[geandert] = baustellenListe.pop(zuaendern)
+            baustellendatenspeichern(baustellenListe)
+            materialAnzeigen(baustellenListe,baustellenInput)
+    elif sicherheitsfrage == "N":
+        aendernabfragen(baustellenListe,baustellenInput)
+
+def materialnamenaendern(baustellenListe,baustellenInput,zuaendern,geandert,sicherheitsfrage,materialname):
+    if sicherheitsfrage == "J":
+        if zuaendern in baustellenListe[baustellenInput]["Material"]:
+            baustellenListe[baustellenInput]["Material"][geandert] = baustellenListe[baustellenInput]["Material"].pop(zuaendern) 
+            baustellendatenspeichern(baustellenListe)
+            materialAnzeigen(baustellenListe,baustellenInput)
+    elif sicherheitsfrage == "N":
+        aendernabfragen(baustellenListe,baustellenInput)
+   
+def mengenaendern(baustellenListe,baustellenInput,zuaendern,geandert,sicherheitsfrage,materialname):
+    if sicherheitsfrage == "J":
+        baustellenListe[baustellenInput]["Material"][materialname]["Menge"] = int(geandert)
         baustellendatenspeichern(baustellenListe)
+        materialAnzeigen(baustellenListe,baustellenInput)
+    elif sicherheitsfrage == "N":
+        aendernabfragen(baustellenListe,baustellenInput)
 
 
 def zurueck():
-    abfragezurueck = input("Möchtest du zurück ins hauptmenue? (J/N)")
+    abfragezurueck = input("Möchtest du zurück ins hauptmenue? (J/N): ")
     if abfragezurueck == "J":
         menueverweis()
     elif abfragezurueck == "N":
